4a574366253585002441036ac80faf3d20bbc829
Kaesa Lyrih <50957602+lyrihkaesa@users.noreply.github.com>
2024-07-29 12:07:37 +0700

    add: User SoftDelete #2

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -18,6 +19,16 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'phone',
+        'avatar',
+        'role',
+        'rt',
+        'rw',
+        'village',
+        'balance',
+        'total_organic_weight',
+        'total_inorganic_weight',
+        'total_waste_weight',
         'email',
         'password',
     ];
