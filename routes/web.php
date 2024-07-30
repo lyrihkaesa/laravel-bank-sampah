046a55cd8b69a7aaf47aa8ea822ee9686092af69
Kaesa Lyrih <50957602+lyrihkaesa@users.noreply.github.com>
2024-07-30 14:59:36 +0700

    fix: auth redirect login #4

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/login', function () {
+    return redirect(route('filament.admin.auth.login'));
+})->name('login');
+
 Route::get('/', function () {
-    return view('welcome');
+    return '404';
 });
